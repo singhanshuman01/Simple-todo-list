2510f5ead27e536b9a57352d0fad81406ef346e5
singhanshuman01 <singhanshuman592@gmail.com>
2025-10-13 11:39:20 +0530

    Updated the sql schema

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 CREATE TABLE items(
     id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL
+    title TEXT NOT NULL,
+    target VARCHAR(30)
 )
 
-INSERT INTO items(title) VALUES('Learn NodeJS')('Be a backend Developer');
+INSERT INTO items(title,target) VALUES('Learn NodeJS','Today')('Be a backend Developer', 'Tomorrow');
